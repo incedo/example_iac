51405a2f5b3da11f3a7f79573bc435b6eff22d4c
kees <kees@incedo.nl>
2023-03-31 16:28:16 +0200

    start with a more basic one

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-## github actions
+# Infrastructure of Example
+This is part of the example project to show how to setup Azure in combination with 
+- static web app support
+
+## dependencies to run
+- azure cli -> https://developer.hashicorp.com/terraform/tutorials/azure-get-started/azure-build
+
+
+## newbie az stuff
+- az login
+- az account set --subscription "<SUBSCRIPTION_ID>"
+### first time setup
+- az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/<SUBSCRIPTION_ID>"
+
+
+## github actions used
 - https://github.com/marketplace/actions/hashicorp-setup-terraform
--- main.tf
+++ main.tf
@@ -1,47 +1,20 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "static_app_rg" {
-  name     = "my-static-web-app-rg"
-  location = "eastus"
-}
-
-resource "azurerm_app_service_plan" "static_app_plan" {
-  name                = "my-static-web-app-plan"
-  location            = "${azurerm_resource_group.static_app_rg.location}"
-  resource_group_name = "${azurerm_resource_group.static_app_rg.name}"
-  sku {
-    tier = "Free"
-    size = "F1"
-  }
-}
-
-resource "azurerm_storage_account" "static_app_storage" {
-  name                     = "mystaticwebappstorage"
-  resource_group_name      = "${azurerm_resource_group.static_app_rg.name}"
-  location                 = "${azurerm_resource_group.static_app_rg.location}"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_static_site" "static_site" {
-  name                = "my-static-web-app"
-  location            = "${azurerm_resource_group.static_app_rg.location}"
-  resource_group_name = "${azurerm_resource_group.static_app_rg.name}"
-  app_service_plan_id  = "${azurerm_app_service_plan.static_app_plan.id}"
-  repository_url      = "https://github.com/myusername/my-static-web-app"
-  branch              = "main"
-  app_settings = {
-    "WEBSITE_NODE_DEFAULT_VERSION" = "14"
-    "WEBSITE_RUN_FROM_PACKAGE"     = "1"
-  }
-  azure_blob_storage {
-    account_name      = "${azurerm_storage_account.static_app_storage.name}"
-    container_name    = "$web"
-    access_key        = "${azurerm_storage_account.static_app_storage.primary_access_key}"
-    cdn_enabled       = true
-    cdn_profile_name  = "my-cdn-profile"
-    cdn_endpoint_name = "my-cdn-endpoint"
-  }
+resource "azurerm_resource_group" "rg" {
+  name     = "devResourceGroup"
+  location = "westeurope"
 }
